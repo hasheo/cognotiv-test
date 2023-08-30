71c6b169f47619f0fcf136c40df47e5828e77460
hashfi <hashfirasis91@gmail.com>
2023-08-30 16:46:12 +0700

    added arrow icon

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,14 +3,27 @@
 </script>
 
 <template>
-  <div class="relative group w-6/12 m-auto ">
+  <div class="relative group w-6/12 m-auto">
     <div class="absolute text-9xl text-yellow-400 font-bold z-10 group-hover:scale-110" style="top: -50px; left:-100px">We Create</div>
     <div class="grayscale hover:grayscale-0 overflow-hidden">
       <img class="object-cover transition ease-in-out group-hover:scale-110" src="../assets/lightbulb2.jpeg">
     </div>
     <div class="relative">
-      <div class="absolute text-7xl transition text-yellow-400 font-bold z-10 uppercase group-hover:translate-x-full -translate-x-full" style="bottom: -30px; left: -100%;">awesome things</div>
-      <div></div>
+      <div 
+        class="absolute text-7xl transition text-yellow-400 font-bold z-10 uppercase group-hover:translate-x-full -translate-x-full" 
+        style="bottom: -30px; left: -100%;"
+      >
+        awesome things
+      </div>
+      <div
+        class="absolute text-7xl transition text-yellow-400 font-bold z-10 uppercase group-hover:-translate-x-full translate-x-full"
+        style="bottom: -30px; right: -50%;"
+      >
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+      </svg>
+
+      </div>
     </div>
   </div>
   
