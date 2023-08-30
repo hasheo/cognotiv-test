3cea050b1827b73e27639d0c493ee4509f323845
hashfi <hashfirasis91@gmail.com>
2023-08-30 13:34:48 +0700

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="relative group w-6/12 m-auto ">
+    <div class="absolute text-9xl text-yellow-400 font-bold z-10 group-hover:scale-110">We Create</div>
+    <div class="grayscale hover:grayscale-0 overflow-hidden">
+      <img class="object-cover transition ease-in-out group-hover:scale-110" src="../assets/lightbulb2.jpeg">
+    </div>
+    <div class="invisible group-hover:visible">Hello this is invisible</div>
+  </div>
+  
+</template>
