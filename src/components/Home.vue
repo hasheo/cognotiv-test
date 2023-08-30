fc8dea0b15088e7f43399171ed415e4fd2368d42
hashfi <hashfirasis91@gmail.com>
2023-08-30 14:38:57 +0700

    fix translation

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,11 +4,14 @@
 
 <template>
   <div class="relative group w-6/12 m-auto ">
-    <div class="absolute text-9xl text-yellow-400 font-bold z-10 group-hover:scale-110">We Create</div>
+    <div class="absolute text-9xl text-yellow-400 font-bold z-10 group-hover:scale-110" style="top: -50px; left:-100px">We Create</div>
     <div class="grayscale hover:grayscale-0 overflow-hidden">
       <img class="object-cover transition ease-in-out group-hover:scale-110" src="../assets/lightbulb2.jpeg">
     </div>
-    <div class="invisible group-hover:visible">Hello this is invisible</div>
+    <div class="relative">
+      <div class="absolute text-7xl transition text-yellow-400 font-bold z-10 uppercase group-hover:translate-x-full -translate-x-full" style="bottom: -30px; left: -100%;">awesome things</div>
+      <div></div>
+    </div>
   </div>
   
 </template>
